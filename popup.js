93a829c2c2c3aa89024d6165ec21a4c6a0886c90
furnellejulien <julien.furnelle@student.vinci.be>
2022-11-22 15:44:27 +0100

    THY SHALL NOT WORK

--- popup.js
+++ popup.js
@@ -134,6 +134,26 @@ function handleModifiyLien() {
   });
 }
 
+
+let THY_SHALL_NOT_WORK = true
+let blocked_url = ["youtube.com","moodle.vinci.be"]
+chrome.storage.sync.set({blocked_url});
+const buttonTSNW = document.getElementById("THYSHALLNOTWORK");
+function activeThyShallNotWork() {
+  THY_SHALL_NOT_WORK = !THY_SHALL_NOT_WORK
+  chrome.storage.sync.set({THY_SHALL_NOT_WORK})
+  console.log(THY_SHALL_NOT_WORK)
+}
+buttonTSNW.addEventListener("click", activeThyShallNotWork);
+const buttonABU = document.getElementById("saveUrlToBlock");
+function addBlockedUrl() {
+  const url = document.getElementById("urlToBlock").value
+  blocked_url.push(url)
+  chrome.storage.sync.set({blocked_url})
+}
+buttonABU.addEventListener("click", addBlockedUrl)
+
+
 SavePerso.addEventListener("click", handlePersoNewTab);
 activeImage.addEventListener("click", handleActivate);
 desactivateImage.addEventListener("click", handleDesactivate);
